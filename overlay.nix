b9d7edbf795ccd75bdd27d36733a1a7d8ddd8fc7
wholikeel <mail@michaellepera.xyz>
2024-09-06 19:05:24 +0930

    feat: add random typing issues fix

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,16 @@ final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
       pygobject-stubs = python-prev.pygobject-stubs.overrideAttrs (old-attrs: {
-        patches = old-attrs.patches or [ ] ++ [ ./gtk4layershell.patch ];
+        patches = old-attrs.patches or [ ] ++ (
+          let
+            patch-dir = ./patches;
+            dir-contents = builtins.readDir patch-dir;
+            files = prev.lib.filterAttrs (_: v: v == "regular") dir-contents;
+            patch-names = prev.lib.getAttrs files;
+            patches = map (n: patch-dir + n) patch-names;
+          in
+          patches
+        );
       });
     })
   ];
