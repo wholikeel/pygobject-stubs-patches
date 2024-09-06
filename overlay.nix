ca67b93f9cf04553e46318f516e29568ee6a09e2
wholikeel <mail@michaellepera.xyz>
2024-09-06 22:15:35 +0930

    fix: function instead of set

--- overlay.nix
+++ overlay.nix
@@ -2,16 +2,10 @@ final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (python-final: python-prev: {
       pygobject-stubs = python-prev.pygobject-stubs.overrideAttrs (old-attrs: {
-        patches = old-attrs.patches or [ ] ++ (
-          let
-            patch-dir = ./patches;
-            dir-contents = builtins.readDir patch-dir;
-            files = prev.lib.filterAttrs (_: v: v == "regular") dir-contents;
-            patch-names = prev.lib.getAttrs files;
-            patches = map (n: patch-dir + n) patch-names;
-          in
-          patches
-        );
+        patches = old-attrs.patches or [ ] ++ [
+          ./patches/gtk4layershell.patch
+          ./patches/random.patch
+        ];
       });
     })
   ];
